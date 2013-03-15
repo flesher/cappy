eb978f70ac694fab260ee94f1dcd7351a188118d
Matt Flesher <matt.flesher@gmail.com>
2013-03-15 09:56:22 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cappy::Application.config.secret_token = 'c2306be3673f01f9406fd34d7b1a9a38c880077689c2260893834b005a1caf0a7024b41ef1ca087ed2eda3e403e8bb3a77dc8c1cc6a28ee7eb03624cedd317c4'
